db9ede2b91537388e62c4e891c0e1863919cd9ae
Mateus Santos <m.santos.azeve@gmail.com>
2022-03-30 21:33:17 -0300

    arquivos de configuracoes gerados

---
+++ app/src/main/java/br/edu/unicarioca/calculator_aps_da_moveis/MainActivity.java
@@ -0,0 +1,14 @@
+package br.edu.unicarioca.calculator_aps_da_moveis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
