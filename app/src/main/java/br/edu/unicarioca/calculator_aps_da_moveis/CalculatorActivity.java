8f5f60b4b6a6a25c63a6418d6f6d13c07c86501f
Mateus Santos <m.santos.azeve@gmail.com>
2022-04-01 09:32:23 -0300

    adicionado assinatura das funcoes

--- app/src/main/java/br/edu/unicarioca/calculator_aps_da_moveis/CalculatorActivity.java
+++ app/src/main/java/br/edu/unicarioca/calculator_aps_da_moveis/CalculatorActivity.java
@@ -3,6 +3,7 @@ package br.edu.unicarioca.calculator_aps_da_moveis;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class CalculatorActivity extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class CalculatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+    }
+
+    public void sumOperation(View view) {
+    }
+
+    public void subOperation(View view) {
+    }
+
+    public void multiOperation(View view) {
+    }
+
+    public void divOperation(View view) {
     }
 }
