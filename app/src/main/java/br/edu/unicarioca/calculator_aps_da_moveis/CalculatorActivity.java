07bb05497bab6a99d86c9b3582439cf48974ff19
Mateus Santos <m.santos.azeve@gmail.com>
2022-04-04 15:49:28 -0300

    ajustes nas operacacoes matematicas

--- app/src/main/java/br/edu/unicarioca/calculator_aps_da_moveis/CalculatorActivity.java
+++ app/src/main/java/br/edu/unicarioca/calculator_aps_da_moveis/CalculatorActivity.java
@@ -11,46 +11,69 @@ public class CalculatorActivity extends AppCompatActivity {
     private EditText etNum1;
     private EditText etNum2;
     private TextView tvResult;
+    private TextView tvOperationSignal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
 
-        etNum1 = (EditText) findViewById(R.id.firstNumber);
-        etNum2 = (EditText) findViewById(R.id.secondNumber);
-        tvResult = (TextView) findViewById(R.id.tvResult);
+
     }
 
     public void sumOperation(View view) {
+        etNum1 = (EditText) findViewById(R.id.firstNumber);
+        etNum2 = (EditText) findViewById(R.id.secondNumber);
+        tvResult = (TextView) findViewById(R.id.tvResult);
+        tvOperationSignal = (TextView) findViewById(R.id.tvOperationSignal);
+
         int num1 = Integer.parseInt(etNum1.getText().toString());
         int num2 = Integer.parseInt(etNum2.getText().toString());
         int resultSum = num1 + num2;
 
         tvResult.setText(String.valueOf(resultSum));
+        tvOperationSignal.setText("+");
     }
 
     public void subOperation(View view) {
+        etNum1 = (EditText) findViewById(R.id.firstNumber);
+        etNum2 = (EditText) findViewById(R.id.secondNumber);
+        tvResult = (TextView) findViewById(R.id.tvResult);
+        tvOperationSignal = (TextView) findViewById(R.id.tvOperationSignal);
+
         int num1 = Integer.parseInt(etNum1.getText().toString());
         int num2 = Integer.parseInt(etNum2.getText().toString());
         int resultSum = num1 - num2;
 
         tvResult.setText(String.valueOf(resultSum));
+        tvOperationSignal.setText("-");
     }
 
     public void multiOperation(View view) {
+        etNum1 = (EditText) findViewById(R.id.firstNumber);
+        etNum2 = (EditText) findViewById(R.id.secondNumber);
+        tvResult = (TextView) findViewById(R.id.tvResult);
+        tvOperationSignal = (TextView) findViewById(R.id.tvOperationSignal);
+
         int num1 = Integer.parseInt(etNum1.getText().toString());
         int num2 = Integer.parseInt(etNum2.getText().toString());
         int resultSum = num1 * num2;
 
         tvResult.setText(String.valueOf(resultSum));
+        tvOperationSignal.setText("*");
     }
 
     public void divOperation(View view) {
+        etNum1 = (EditText) findViewById(R.id.firstNumber);
+        etNum2 = (EditText) findViewById(R.id.secondNumber);
+        tvResult = (TextView) findViewById(R.id.tvResult);
+        tvOperationSignal = (TextView) findViewById(R.id.tvOperationSignal);
+
         int num1 = Integer.parseInt(etNum1.getText().toString());
         int num2 = Integer.parseInt(etNum2.getText().toString());
         int resultSum = num1 / num2;
 
         tvResult.setText(String.valueOf(resultSum));
+        tvOperationSignal.setText("/");
     }
 }
